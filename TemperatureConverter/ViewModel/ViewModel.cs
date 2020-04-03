6452e01c23b0fb0b23b3e54ce4bdc0c14f674342
rafael <rafael.polfliet@student.ucll.be>
2020-04-03 23:48:28 +0200

    temperature-scale-control

--- TemperatureConverter/ViewModel/ViewModel.cs
+++ TemperatureConverter/ViewModel/ViewModel.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Cells;
 using Model;
 
@@ -51,10 +52,59 @@ namespace ViewModel
             this.temperatureScale = temperatureScale;
 
             this.Temperature = this.parent.TemperatureInKelvin.Derive(kelvin => temperatureScale.ConvertFromKelvin(kelvin),other => temperatureScale.ConvertToKelvin(other));
+
+
+            var minimum = temperatureScale.ConvertFromKelvin(0);
+            var maximum = temperatureScale.ConvertToKelvin(1000);
+
+            this.Add = new AddCommand(this.Temperature,1,minimum,maximum);
+            this.Sub = new AddCommand(this.Temperature, -1,minimum, maximum);
         }
 
         public string Name => temperatureScale.Name;
 
         public Cell<double> Temperature { get; }
+
+        public ICommand Add { get; }
+
+        public ICommand Sub { get; }
+    }
+
+
+    public class AddCommand : ICommand
+    {
+        private readonly Cell<double> cell;
+
+        private readonly int delta;
+
+
+        private readonly double minimum;
+
+        private readonly double maximimum;
+
+        public AddCommand(Cell<double> cell, int delta, double minimum, double maximimum)
+        {
+            this.cell = cell;
+            this.delta = delta;
+            this.minimum = minimum;
+            this.maximimum = maximimum;
+
+            this.cell.PropertyChanged += (sender, args) => CanExecuteChanged(this, new EventArgs());
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+
+        public bool CanExecute(object parameter)
+        {
+            var newValue = cell.Value + delta;
+
+            return minimum <= newValue && newValue <= maximimum;
+        }
+
+        public void Execute(object parameter)
+        {
+            cell.Value = Math.Round(cell.Value + delta);
+        }
     }
 }
