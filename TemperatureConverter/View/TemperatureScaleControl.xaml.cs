ce3b03f5ea74d838ac9c5b8aa7307e1f0a67f261
rafael <rafael.polfliet@student.ucll.be>
2020-04-04 00:04:52 +0200

    done

--- TemperatureConverter/View/TemperatureScaleControl.xaml.cs
+++ TemperatureConverter/View/TemperatureScaleControl.xaml.cs
@@ -24,5 +24,27 @@ namespace View
         {
             InitializeComponent();
         }
+
+        public string Header
+        {
+            get { return (string)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register("Header", typeof(string), typeof(TemperatureScaleControl), new PropertyMetadata(""));
+
+
+
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(double), typeof(TemperatureScaleControl), new PropertyMetadata(0.0));
+
+
     }
 }
