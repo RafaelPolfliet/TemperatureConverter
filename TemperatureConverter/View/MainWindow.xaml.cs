c6ccd4e528492ce3f3874257cbe3e695a917a411
rafael <rafael.polfliet@student.ucll.be>
2020-04-03 16:49:44 +0200

    nothing

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -25,57 +25,24 @@ namespace View
         {
             InitializeComponent();
         }
-
-       /* private void SliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            var kelvin = slider.Value;
-            var celsius = kelvin - 273.15;
-            var fahrenheit = celsius * 1.8 + 32;
-
-            var kelvinString = kelvin.ToString();
-            var fahrenheitString = fahrenheit.ToString();
-            var celsiusString = celsius.ToString();
-
-            fahrenheitTextBox.Text = fahrenheitString;
-            celsiusTextBox.Text = celsiusString;
-        }*/
     }
 
-    public class CelsiusConverter : IValueConverter
+    public class TemperatureConverter : IValueConverter
     {
+        public ITemperatureScale TemperatureScale { get; set; }
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var kelvin = (double)value;
-            var celsius = kelvin - 273.15;
 
-            return celsius;
+            return this.TemperatureScale.ConvertFromKelvin(kelvin).ToString();
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var celsius = double.Parse((string)value);
-            var kelvin = celsius + 273.15;
+            var temperature = double.Parse((string)value);
 
-            return kelvin;
-        }
-    }
-
-    public class FahrenheitConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var kelvin = (double)value;
-            var fahrenheit = kelvin * 9 / 5 - 459.67;
-
-            return fahrenheit;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var fahrenheit = double.Parse((string)value);
-            var kelvin = (fahrenheit + 459.67)*5/9;
-
-            return kelvin;
+            return this.TemperatureScale.ConvertToKelvin(temperature);
         }
     }
 }
